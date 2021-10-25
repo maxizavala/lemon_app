d26b6b75036255dc13a0b19ee73e914905080ad8
maxizavala <maximiliano.zavala@davinci.edu.ar>
2021-10-24 22:26:06 -0300

    Creacion del proyecto

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Coder! Soy Maximiliano Zavala</Text>
       <StatusBar style="auto" />
     </View>
   );
